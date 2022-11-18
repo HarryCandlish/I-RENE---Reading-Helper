ad37d7f2152cb3bbc19e29794b5ebdf93dd6b29c
Harry Candlish <hcnd90@gmail.com>
2022-11-19 08:16:03 +1300

    added component and page files

---
+++ src/pages/add_book/AddBook.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './addbook.css'
+
+const AddBook = () => {
+  return (
+    <div>AddBook</div>
+  )
+}
+
+export default AddBook
---
+++ src/pages/edit_book/EditBook.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './editbook.css'
+
+const EditBook = () => {
+  return (
+    <div>EditBook</div>
+  )
+}
+
+export default EditBook
